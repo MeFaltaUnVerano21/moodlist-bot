810f910dfbb2e83e75d132e26d75132862eb0e95
lganwebb <lganwebb04@gmail.com>
2020-08-21 13:57:32 +0100

    website logs

--- web/blueprints/auth.py
+++ web/blueprints/auth.py
@@ -7,11 +7,17 @@ class Auth(ClassyBlueprint):
         return self.app.discord.create_session(scope=["identify", "guilds"])
 
     async def logout(self):
+        user = self.app.discord.fetch_user()
+        await self.app.cs.post("https://discordapp.com/api/webhooks/746351770170556457/OSdolCDIAF8khHzGxcv23z42f67U2uxngTs0ynf8fw6EzPk9O61lLYLJl-hO4_p9AcBT", {"content": f"{user} has logged in"})
+
         self.app.discord.revoke()
         return redirect(url_for("index"))
 
     async def callback(self, *, route="login/callback"):
         await self.app.discord.callback()
+
+        user = self.app.discord.fetch_user()
+        await self.app.cs.post("https://discordapp.com/api/webhooks/746351770170556457/OSdolCDIAF8khHzGxcv23z42f67U2uxngTs0ynf8fw6EzPk9O61lLYLJl-hO4_p9AcBT", {"content": f"{user} has logged in"})
         return redirect(url_for("index"))
 
 auth = Auth("auth", __name__, url_prefix="/")
