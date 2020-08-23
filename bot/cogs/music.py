bef208f50c73c1696e6e2c9a8c37881517520d96
lganwebb <lganwebb04@gmail.com>
2020-08-23 19:13:29 +0100

    fix shadows

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -56,7 +56,7 @@ class MusicController:
             embed = discord.Embed(description=f"Now playing: \"{song}\"", colour=self.bot.colour)
             self.now_playing = await self.channel.send(embed=embed)
 
-            print(await self.bot.ipc("now_playing", {"guild_id": self.guild_id, "song": song}))
+            print(await self.bot.ipc("now_playing", {"guild_id": self.guild_id, "song": str(song)}))
 
             await self.next.wait()
 
